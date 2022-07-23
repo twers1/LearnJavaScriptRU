b3f9d1a64b6ac746a5b5a57cb5d5d68838bf27f5
Sima Tarverdyan <102418053+twers1@users.noreply.github.com>
2022-07-23 21:20:52 +0300

    Create 2.простенькийкалькулятор.js

---
+++ 2. Объекты: основы/2.простенькийкалькулятор.js
@@ -0,0 +1,26 @@
+/* Условие: 
+Создайте объект calculator (калькулятор) с тремя методами:
+
+read() (читать) запрашивает два значения и сохраняет их как свойства объекта.
+sum() (суммировать) возвращает сумму сохранённых значений.
+mul() (умножить) перемножает сохранённые значения и возвращает результат.
+*/
+// Решение: 
+let calculator = {
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+
+  read() {
+    this.a = +prompt('a?', 0);
+    this.b = +prompt('b?', 0);
+  }
+};
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
